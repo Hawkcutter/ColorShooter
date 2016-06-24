27dd97aaf86d48e75b7ed2af0351125aa2ddda6a
kaibo <kai.bornemann@st.ovgu.de>
2016-06-24 23:53:09 +0200

    Hitting enemies, restructure Projectile.cs

--- ColorShooter/Assets/Scripts/Projectiles/Projectile.cs
+++ ColorShooter/Assets/Scripts/Projectiles/Projectile.cs
@@ -19,62 +19,25 @@ public abstract class Projectile : MonoBehaviour
 
     protected abstract void OnShotFired(Vector2 startPos, Vector2 direction);
 
-    protected abstract void OnUpdate();
-    protected abstract void OnFixedUpdate();
-    protected abstract void OnShotDestroyed();
 
-    protected abstract void OnHitEnter(Collider2D collider);
-    protected abstract void OnHitUpdate(Collider2D collider);
-    protected abstract void OnHitExit(Collider2D collider);
 
-    void Start()
+    public void Init(Vector2 startPos, Vector2 direction, ColorKey colorKey, bool isPlayerBullet)
     {
         if (!projectileRoot)
         {
             projectileRoot = gameObject;
         }
-    }
 
-    public void Init(Vector2 startPos, Vector2 direction, ColorKey colorKey, bool isPlayerBullet)
-    {
         IsPlayerBullet = isPlayerBullet;
         ColorKey = colorKey;
-        OnShotFired(startPos, direction);
+
 
         if (isPlayerBullet)
             targetLayer = GameManager.Instance.GetEnemyLayer();
 
         else
             targetLayer = GameManager.Instance.GetPlayerLayer();
-    }
 
-    void Update()
-    {
-        OnUpdate();
-    }
-
-    void FixedUpdate()
-    {
-        OnFixedUpdate();
-    }
-
-    void OnDestroy()
-    {
-        OnShotDestroyed();
-    }
-
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        OnHitEnter(collider);
-    }
-
-    void OnTriggerStay2D(Collider2D collider)
-    {
-        OnHitUpdate(collider);
-    }
-
-    void OnTriggerExit2D(Collider2D collider)
-    {
-        OnHitExit(collider);
+        OnShotFired(startPos, direction);
     }
 }
